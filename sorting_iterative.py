064a2ba09ceddc25b07e757d542fa0b6c37976bf
Nolan <noltron000@gmail.com>
2019-10-28 13:37:41 -0700

    [add] bubble_sort

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -7,11 +7,11 @@ def is_sorted(items):
 	'''
 	# Check that all adjacent items are in order.
 	for index, current_item in enumerate(items):
-		# The first item in items gets a free pass.
+		# The first item gets a free pass.
 		if index != 0:
 			previous_item = items[index - 1]
 			# Return early if items is not in order.
-			if not previous_item <= current_item:
+			if previous_item > current_item:
 				return False
 	# If the loop finishes, then its in order.
 	else:
@@ -25,14 +25,26 @@ def bubble_sort(items):
 		TODO: Running time: ??? Why and under what conditions?
 		TODO: Memory usage: ??? Why and under what conditions?
 	'''
-	# ==TODO==
+	# Initialize loop variable as `> 0`.
+	swaps = 1
 	# Repeat until all items are in sorted order.
-
-	# ==TODO==
-	# Swap adjacent items that are out of order.
-
-	pass
-
+	while swaps > 0:
+		# Reset number of swaps for this iteration.
+		swaps = 0
+		# Loop through every element.
+		for index in range(len(items)):
+			# The first item gets a free pass.
+			if index != 0:
+				current_item = items[index]
+				previous_item = items[index - 1]
+				# Check if adjacent items are out of order.
+				if previous_item > current_item:
+					# Swap adjacent items that are out of order.
+					items[index] = previous_item
+					items[index - 1] = current_item
+					# Mark number of swaps.
+					swaps += 1
+	return items
 
 def selection_sort(items):
 	'''
