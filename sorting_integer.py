f962c02e67acc93e2ac264ab285bcd17e322ece7
Nolan <noltron000@gmail.com>
2019-11-04 15:48:27 -0800

    [fmt] sorting_integer

--- sorting_integer.py
+++ sorting_integer.py
@@ -1,47 +1,57 @@
 def counting_sort(numbers):
 	'''
-		Sort given numbers (integers) by counting occurrences of each number,
-		then looping over counts and copying that many numbers into output list.
+		Sort given numbers (integers) by counting occurrences
+		of each number, then looping over counts and copying
+		that many numbers into output list.
 		TODO: Running time: ??? Why and under what conditions?
 		TODO: Memory usage: ??? Why and under what conditions?
 	'''
 	# ==TODO==
-	# Find range of given numbers (minimum and maximum integer values)
+	# Find range of given numbers (min/max integer values).
 
 	# ==TODO==
-	# Create list of counts with a slot for each number in input range
+	# Create list of counts with a slot
+	# for each number in input range.
 
 	# ==TODO==
-	# Loop over given numbers and increment each number's count
+	# Loop over given numbers and
+	# increment each number's count.
 
 	# ==TODO==
-	# Loop over counts and append that many numbers into output list
+	# Loop over counts and append
+	# that many numbers into output list.
 
 	# ==FIXME==
-	# Improve this to mutate input instead of creating new output list
+	# Improve this to mutate input
+	# instead of creating new output list.
 
 
 def bucket_sort(numbers, num_buckets=10):
 	'''
-		Sort given numbers by distributing into buckets representing subranges,
-		then sorting each bucket and concatenating all buckets in sorted order.
+		Sort given numbers by distributing into buckets
+		representing subranges, then sorting each bucket
+		and concatenating all buckets in sorted order.
 		TODO: Running time: ??? Why and under what conditions?
 		TODO: Memory usage: ??? Why and under what conditions?
 	'''
 	# ==TODO==
-	# Find range of given numbers (minimum and maximum values)
+	# Find range of given numbers (min/max values)
 
 	# ==TODO==
-	# Create list of buckets to store numbers in subranges of input range
+	# Create list of buckets to store
+	# numbers in subranges of input range.
 
 	# ==TODO==
-	# Loop over given numbers and place each item in appropriate bucket
+	# Loop over given numbers and
+	# place each item in appropriate bucket.
 
 	# ==TODO==
-	# Sort each bucket using any sorting algorithm (recursive or another)
+	# Sort each bucket using any sorting algorithm.
 
 	# ==TODO==
-	# Loop over buckets and append each bucket's numbers into output list
+	# Loop over buckets and append
+	# each bucket's numbers into output list.
 
 	# ==FIXME==
-	# Improve this to mutate input instead of creating new output list
+	# Improve this to mutate input
+	# instead of creating new output list.
