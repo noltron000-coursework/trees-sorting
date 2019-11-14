2861c83ff62dfcb8a6175a28f4e83100871c73c1
Nolan Kovacik <noltron000@gmail.com>
2019-11-14 10:16:21 -0800

    [add] integer sorting baby-steps

--- sorting_integer.py
+++ sorting_integer.py
@@ -39,12 +39,33 @@ def bucket_sort(numbers, num_buckets=10):
 		TODO: Running time: ??? Why and under what conditions?
 		TODO: Memory usage: ??? Why and under what conditions?
 	'''
-	# ==TODO==
-	# Find range of given numbers (min/max values).
+	# Find range of given numbers (min/max integer values).
+	minimum = min(numbers)
+	maximum = max(numbers)
 
-	# ==TODO==
+	# 0 > 0/1 layers
+	# 1 > 1 layer
+	# 10 > 2 layers
+	# 100 > 3 layers
+	layer = 1
+	# is 10 ** (1 - 1) <= 10 ?
+	# 10 > 1.
+	# 	at least 1 layer.
+	# is 10 ** (2 - 1) <= 10 ?
+	# 10 == 10
+	# 	at least 2 layers.
+	# is 10 ** (3 - 1) <= 10 ?
+	# 10 < 100
+	# 	not 3 or more layers.
+	while num_buckets ** layer <= maximum:
+		layer += 1
+	assert(num_buckets ** layer > maximum)
+
 	# Create list of buckets to store
 	# numbers in subranges of input range.
+	buckets = [None] * num_buckets
+	for number in numbers:
+		if number % num_buckets ** layer
 
 	# ==TODO==
 	# Loop over given numbers and
@@ -60,3 +81,11 @@ def bucket_sort(numbers, num_buckets=10):
 	# ==FIXME==
 	# Improve this to mutate input
 	# instead of creating new output list.
+
+
+bucket_sort([0])
+bucket_sort([1])
+bucket_sort([5])
+bucket_sort([9])
+bucket_sort([10])
+bucket_sort([11])
